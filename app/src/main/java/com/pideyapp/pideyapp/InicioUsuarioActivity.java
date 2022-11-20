d960f348cf8498572f7d662af3593c0a8b72800a
gissel.ariza <gissel.ariza@o365.unab.edu.co>
2022-11-19 20:01:54 -0500

    git add.

---
+++ app/src/main/java/com/pideyapp/pideyapp/InicioUsuarioActivity.java
@@ -0,0 +1,14 @@
+package com.pideyapp.pideyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InicioUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_usuario);
+    }
+}
